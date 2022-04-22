fa91784a9f6231d89060e1a92104bb36c8266af1
minghui.shen <13030365596@163.com>
2022-04-22 09:08:30 +0800

    proto包引入问题

--- build_proto.sh
+++ build_proto.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 protoc -I ./protobuf \
-       --go_out ../ \
-       --go-grpc_out ../ \
+       --go_out=../../ \
+       --go-grpc_out=../../ \
        ./protobuf/langya_platform_app.proto
 
 protoc -I ./protobuf \
-       --go_out ../ \
-       --go-grpc_out ../ \
+       --go_out=../../  \
+       --go-grpc_out=../../ \
        ./protobuf/langya_platform_common.proto
